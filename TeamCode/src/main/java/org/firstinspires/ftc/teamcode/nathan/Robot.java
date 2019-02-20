9c4a5d96d4bc57781556eef95efd9fecb93701b8
bigmich@outlook.com <bookworm:)>
2019-02-19 18:34:35 -0800

    Changed values for lift mechanism.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nathan/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nathan/Robot.java
@@ -29,7 +29,7 @@ public class Robot {
      */
     public void liftUp()
     {
-        lift.setTargetPosition(7100);
+        lift.setTargetPosition(3160);
         while(lift.isBusy()) {
             lift.setPower(1);
         }
